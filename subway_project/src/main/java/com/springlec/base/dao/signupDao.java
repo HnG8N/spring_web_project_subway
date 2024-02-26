26d91e42822501e15b49a4db31c10a3c348d3847
kimsor1 <imoiksr@gmail.com>
2024-02-26 13:54:02 +0900

    로그인 기능 구현 (로그인 시 헤더 변경되는 것 추가)

--- subway_project/src/main/java/com/springlec/base/dao/signupDao.java
+++ subway_project/src/main/java/com/springlec/base/dao/signupDao.java
@@ -5,5 +5,6 @@ public interface signupDao {
 	public void signup(String mid, String mpw, String mname, String maddress, String mtel, String mbirth, String memail)
 			throws Exception;
 	public int checkId(String mid) throws Exception;
+	public String checkPw(String mid) throws Exception;
 	public int checkEmail(String memail) throws Exception;
 }
